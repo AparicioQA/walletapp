af5546453d505d20e0b05ee64a0b1c11bcfa30d4
Brian Quesada <aparicio30@protonmail.com>
2022-07-02 16:46:27 -0600

    Se cambio a https la url del api

--- src/helpers/walletApi.js
+++ src/helpers/walletApi.js
@@ -1,6 +1,6 @@
 async function walletApi(endpoint, method = 'GET', data = {}) {
     const token = localStorage.getItem('token') != null ? localStorage.getItem('token') : '';
-    return await fetch(`http://brian-walletapi.azurewebsites.net/${endpoint}`, {
+    return await fetch(`https://brian-walletapi.azurewebsites.net/${endpoint}`, {
         // credentials: 'include',
         method: method,
         headers: {
